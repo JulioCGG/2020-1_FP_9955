e9843fc1c4ace5a9f1508e085267cdccb0ae66ee
JulioCGG <54299232+JulioCGG@users.noreply.github.com>
2019-10-22 19:09:24 -0500

    Update main.c

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,14 +1,18 @@
 #include  <stdio.h>
 #include  <stdlib.h>
 int main(int argc, char * argv[]){
+	//se declara las variables
 	int lista[argc], suma=0;
 	float promedio;
+	//se inicia el for para convertir los caracteres que ingresen a numeros
 	for (int i=1; i<argc; i++){
 	lista[i]=atoi(argv[i]);
 	}
+	//se inicia otro for para realizar la suma de los numeros
 	for (int i=0; i<argc; i++){
 	suma=suma+lista[i];
 	}
+	//se calcula el promedio
 	promedio=(float)suma/(argc-1);
 	printf("%f\n", promedio);
 	return 0;
